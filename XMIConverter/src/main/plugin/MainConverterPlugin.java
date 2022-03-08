ae0e393677071cbf293fc9c18102340c51000a65
juliarobles <juliarobles@uma.es>
2022-03-09 00:05:56 +0100

    Plugin running only with a println. Added use-gui jar.

--- XMIConverter/src/main/plugin/MainConverterPlugin.java
+++ XMIConverter/src/main/plugin/MainConverterPlugin.java
@@ -1,13 +1,17 @@
 package main.plugin;
 
-//import org.tzi.use.gui.main.MainWindow;
-import org.tzi.use.api.UseSystemApi;
+import org.tzi.use.runtime.gui.IPluginAction;
+import org.tzi.use.runtime.gui.IPluginActionDelegate;
+
+/*
+import org.tzi.use.gui.main.MainWindow;
 import org.tzi.use.runtime.gui.IPluginAction;
 import org.tzi.use.runtime.gui.IPluginActionDelegate;
 import org.tzi.use.main.Session;
 import org.tzi.use.uml.sys.MSystem;
 import org.tzi.use.config.Options;
 import main.model.Generators;
+*/
 
 public class MainConverterPlugin implements IPluginActionDelegate {
 	
@@ -17,17 +21,24 @@ public class MainConverterPlugin implements IPluginActionDelegate {
 
 	@Override
 	public void performAction(IPluginAction pluginAction) {
+		/*
 		// TODO Auto-generated method stub
+		System.out.println("A0");
 		Session curSession = pluginAction.getSession();
+		System.out.println("A1");
 		MSystem curSystem = curSession.system();
-	//	MainWindow curMainWindow = pluginAction.getParent();
+		System.out.println("A2");
+		MainWindow curMainWindow = pluginAction.getParent();
+		System.out.println("A4");
 		
 		String fileName = Options.specFilename;
 		if (fileName != null) {
 			//Generators.fromUSEtoUML(fileName, "C:/Users/julia");
 			System.out.println("Filename: " + fileName);
-		//	curMainWindow.logWriter().println("cannot use kodkod plugin");
+			curMainWindow.logWriter().println("Filename: " + fileName);
 		}
+		*/
+		System.out.println("FUNCIONA!");
 		
 	}
 }
