351a715f8930ca38d637f6c733f3b5d8cb7b214a
juliarobles <juliarobles@uma.es>
2022-03-15 23:36:14 +0100

    Trying to fix dependency errors... Not working...

--- XMIConverter/src/main/plugin/MainConverterPlugin.java
+++ XMIConverter/src/main/plugin/MainConverterPlugin.java
@@ -3,15 +3,13 @@ package main.plugin;
 import org.tzi.use.runtime.gui.IPluginAction;
 import org.tzi.use.runtime.gui.IPluginActionDelegate;
 
-/*
+
 import org.tzi.use.gui.main.MainWindow;
-import org.tzi.use.runtime.gui.IPluginAction;
-import org.tzi.use.runtime.gui.IPluginActionDelegate;
 import org.tzi.use.main.Session;
+import org.tzi.use.uml.mm.MModel;
 import org.tzi.use.uml.sys.MSystem;
-import org.tzi.use.config.Options;
 import main.model.Generators;
-*/
+
 
 public class MainConverterPlugin implements IPluginActionDelegate {
 	
@@ -21,23 +19,21 @@ public class MainConverterPlugin implements IPluginActionDelegate {
 
 	@Override
 	public void performAction(IPluginAction pluginAction) {
-		/*
-		// TODO Auto-generated method stub
-		System.out.println("A0");
-		Session curSession = pluginAction.getSession();
-		System.out.println("A1");
-		MSystem curSystem = curSession.system();
-		System.out.println("A2");
-		MainWindow curMainWindow = pluginAction.getParent();
-		System.out.println("A4");
 		
-		String fileName = Options.specFilename;
+		// TODO Auto-generated method stub
+		
+		Session curSession = pluginAction.getSession();
+		MSystem curSystem = curSession.system();
+		MModel model = curSystem.model();
+		
+		MainWindow curMainWindow = pluginAction.getParent();
+		
+		String fileName = model.filename();
 		if (fileName != null) {
-			//Generators.fromUSEtoUML(fileName, "C:/Users/julia");
+			Generators.fromUSEtoUML(fileName, "C:/Users/julia");
 			System.out.println("Filename: " + fileName);
 			curMainWindow.logWriter().println("Filename: " + fileName);
 		}
-		*/
 		System.out.println("FUNCIONA!");
 		
 	}
